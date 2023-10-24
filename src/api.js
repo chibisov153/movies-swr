c1ad0ac612db66f563ade2b08c91b0c5f88c63c4
chibisov153 <chibisov153@gmail.com>
2023-10-24 19:56:02 +0700

    up data two

---
+++ src/api.js
@@ -0,0 +1,16 @@
+import express, { Router } from 'express'
+import serverless from 'serverless-http'
+
+const app = express()
+
+const router = Router()
+router.get("/", (req, res) => {
+  res.json({
+    hello: "hi!"
+  });
+});
+
+app.use(`/.netlify/functions/api`, router);
+
+module.exports = app;
+module.exports.handler = serverless(app);
